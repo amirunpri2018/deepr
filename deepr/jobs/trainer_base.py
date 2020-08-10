a16c43ec10a7d1a8651dff4dc8e801fad6e31848
Guillaume Genthial <g.genthial@criteo.com>
2020-08-10 17:06:58 +0200

    Add TrainerKeras to train keras model on Yarn

---
+++ deepr/jobs/trainer_base.py
@@ -0,0 +1,33 @@
+"""Trainer Base."""
+
+import logging
+import dataclasses
+
+from abc import abstractmethod
+
+import tensorflow as tf
+
+from deepr.jobs import base
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class TrainerBase(base.Job):
+    """Trainer Base."""
+
+    def run(self):
+        experiment = self.create_experiment()
+        tf.estimator.train_and_evaluate(experiment.estimator, experiment.train_spec, experiment.eval_spec)
+        for exporter in self.exporters:
+            exporter(experiment.estimator)
+        self.run_final_evaluation()
+
+    @abstractmethod
+    def create_experiment(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def run_final_evaluation(self):
+        raise NotImplementedError
